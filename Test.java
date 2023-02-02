bc1796d8583824463aff5e185afbc06b951fa3be
Your Name <you@example.com>
2023-02-02 16:55:10 +0530

    Third  commit

--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
 public class Test{
     public static void main(String[] args) {
-        System.out.println("  prime numers till 20 numbers :");
-        System.out.println("  prime numers till 20 numbers");
+        System.out.println(" armstrong number:");
 
 
 
